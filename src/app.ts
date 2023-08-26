33626fae57f45d1ea8e737afff63f9a75d323988
João <joaopfc2764@gmail.com>
2023-08-25 23:08:10 -0300

    feat: create prisma client instance to example

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'crypto'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    id: randomUUID(),
+    name: 'Teste',
+    email: 'teste@teste.com'
+  }
+})
 
 export const app = fastify()
