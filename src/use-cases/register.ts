a4b21d464e254199a115d47d784f7a254ecbfc95
DevJpFarias <joaopfc2764@gmail.com>
2023-09-22 17:24:00 -0300

    refactor: utilizando tratativa de erro

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,3 +1,4 @@
+import { UserAlreadyExistsError } from "@/errors/user-already-exists-error"
 import { prisma } from "@/lib/prisma"
 import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
@@ -16,7 +17,7 @@ export class RegisterUseCase {
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-    if (userWithSameEmail) throw new Error('Email already exists!')
+    if (userWithSameEmail) throw new UserAlreadyExistsError()
 
     await this.usersRepository.create({
       name,
