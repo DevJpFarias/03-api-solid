78f0ad493c6406dc7c3526fce1726ac93353e64d
DevJpFarias <joaopfc2764@gmail.com>
2023-09-22 16:09:38 -0300

    feat: senha agora é criptografada

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 import { prisma } from '../../lib/prisma'
  
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,11 +12,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     }
   })
 
