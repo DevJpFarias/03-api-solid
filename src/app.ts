f9b67115e643869596e4601cfcfa97769fed9cfe
DevJpFarias <joaopfc2764@gmail.com>
2023-09-22 14:53:29 -0300

    refactor: registrado plugin de rotas do app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { randomUUID } from 'crypto'
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    id: randomUUID(),
-    name: 'Teste',
-    email: 'teste@teste.com'
-  }
-})
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
